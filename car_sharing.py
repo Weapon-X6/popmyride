7add0df990d692698075f80ebaf9f7f0ed777a08
waffe_x20 <argueta_eduardo@yahoo.de>
2022-08-13 15:52:21 -0600

    First commit with basic endpoints

---
+++ car_sharing.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
+
+@app.get("/")
+def willkommen(name):
+    """return a friendly welcome"""
+    return {'message': f"Welcome {name}, to the Car Sharing Service!"}
+
+
+@app.get("/api/cars")
+def get_cars():
+    return db
